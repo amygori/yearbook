e1fafb3c9a7ff9352d0def3983ce10ddad5891db
Sebastian Armano <sebarmano@gmail.com>
2014-07-24 15:50:13 -0400

    First commit

---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,27 @@
+require 'dragonfly'
+
+# Configure
+Dragonfly.app.configure do
+  plugin :imagemagick
+
+  protect_from_dos_attacks true
+  secret "3204796a8c63dd2adef4c33e6509b1c974d393db3c88002a5c70c9b7815a18e4"
+
+  url_format "/media/:job/:name"
+
+  datastore :file,
+    root_path: Rails.root.join('public/system/dragonfly', Rails.env),
+    server_root: Rails.root.join('public')
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware
+
+# Add model functionality
+if defined?(ActiveRecord::Base)
+  ActiveRecord::Base.extend Dragonfly::Model
+  ActiveRecord::Base.extend Dragonfly::Model::Validations
+end
